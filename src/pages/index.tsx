fe032573f0acddb59791f28dac610e2876f32d10
mayanan <mayanan@zhongan.com>
2021-03-05 13:54:26 +0800

    [init] HTML模版

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import styles from './index.less';
+import styles from './index.less'
 
 export default function IndexPage() {
-  return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className={styles.title}>Page index</h1>
+			<p>window.DEPLOY_ENV: {window.DEPLOY_ENV}</p>
+		</div>
+	)
 }
